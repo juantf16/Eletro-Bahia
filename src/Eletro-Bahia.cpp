f1b9c207a268f9c5e98743aab45e359eef1c2af1
juan <juan16foltran@gmail.com>
2025-08-06 18:21:05 -0300

    testes de conexões do projeto

--- src/Eletro-Bahia.cpp
+++ src/Eletro-Bahia.cpp
@@ -1,13 +1,38 @@
 #include <Arduino.h>
 
+const int up = 4;
+const int dow = 5;
+const int selecionar = 15;
+const int temp = 34;
+const int ebulidor = 19;
+const int prep = 23;
+
+int valorup = 0;
+int valordow = 0;
+int valorselect = 0;
+int valortemp = 0;
+
+
 
 void setup() {
-  pinMode(2, OUTPUT);  // Configura o pino 2 como saída
+  Serial.begin (115200);
+  pinMode(up, INPUT);  
+  pinMode(dow, INPUT);
+  pinMode (selecionar, INPUT);
+  pinMode (temp, INPUT);
+  pinMode (ebulidor, OUTPUT);
+  pinMode (prep, OUTPUT);
+
 }
 
 void loop() {
-  digitalWrite(2, HIGH);  // Liga o LED
-  delay(1000);            // Espera 1 segundo
-  digitalWrite(2, LOW);   // Desliga o LED
-  delay(1000);            // Espera 1 segundo
+  digitalWrite (ebulidor, HIGH);
+  delay (100);
+  digitalWrite (ebulidor, LOW);
+  delay (100);
+  digitalWrite (prep, HIGH);
+  delay (100);
+  digitalWrite (prep, LOW);
+  delay(100);
+
 }
